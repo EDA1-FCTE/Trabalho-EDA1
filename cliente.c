23461252068cc5b8c49ba6f3ff4aa2f5ab228c19
Melissa <7yehet@gmail.com>
2026-01-28 23:19:17 -0300

    Adiciona o arquivo cliente.c com a implementação das funções de inicialização da lista de clientes e de cadastro de clientes.

---
+++ cliente.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "cliente.h"
+#include "produto.h"
+
+cliente* cria_lista_clientes(){
+    return NULL;
+}
+
+cliente* cadastrar_cliente(cliente* head){
+    cliente* novo_cliente = (cliente*) malloc(sizeof (cliente));
+    int validaCPF = 0;
+
+    if (novo_cliente == NULL){
+        printf("Não foi possível alocar memória.\n");
+        return head;
+    }
+
+    printf("\n=== Cadastro Novo ===\n");
+
+    while(!validaCPF){
+    printf("Digite o CPF (Apenas numeros):\n");
+    scanf(" %[^\n]", novo_cliente->CPF);
+
+    if (strlen (novo_cliente->CPF) != 11){
+        printf("O CPF deve conter apenas 11 digitos!\n");
+
+    }
+
+    else {
+
+        int somente_numeros = 1;
+
+        for (int i =0; i < 11; i++){
+            if (!isdigit(novo_cliente -> CPF[i])){
+                somente_numeros = 0;
+                break;
+            }
+        }
+
+        if (somente_numeros) {
+            validaCPF = 1;
+        }
+        else{
+            printf("O CPF deve conter apenas numeros, sem pontos ou tracos.\n");
+        }
+    }
+    }
+
+    printf("Digite o nome (Máximo de 100 caracteres):\n");
+    scanf(" %[^\n]", novo_cliente->nome);
+
+    printf("Digite o e-mail:\n");
+    scanf(" %[^\n]", novo_cliente->email);
+
+    printf("Digite o telefone:\n");
+    scanf(" %[^\n]", novo_cliente->telefone);
+
+    printf("Digite a data de nascimento (dd/mm/aaaa):\n");
+    scanf(" %[^\n]", novo_cliente->data_de_nascimento);
+
+    novo_cliente->prox = head;
+
+    printf("Cadastro finalizado!\n");
+
+    return novo_cliente;
+}
+
+/* int main(){
+    
+} */
