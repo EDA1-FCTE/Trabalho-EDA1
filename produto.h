e6823895b8c8cfd5beeb69d69c556c6ea383d3fc
Melissa <7yehet@gmail.com>
2026-01-28 23:15:44 -0300

    Adiciona um arquivo header produto.h com a struct produto e as definições das funções de inicialização da lista de produtos e de cadastro de produtos.

---
+++ produto.h
@@ -0,0 +1,22 @@
+/*  Aquivo cabeçalho com definição da struct PRODUTO e declarações das
+*   funções de CRUD relacionadas à struct produto:
+*   CADASTRAR PRODUTO, BUSCAR PRODUTO, LISTAR PRODUTOS, REMOVER PRODUTOS, EDITAR PRODUTOS
+*
+*/
+
+#ifndef PRODUTOS_H
+#define PRODUTOS_H
+
+typedef struct produto
+{
+    char nome[100];
+    int codigo;
+    float preco;
+    int quantidade;
+    struct produto* prox;
+} produto;
+
+produto* cria_lista_produtos();
+produto* cadastrar_produto(produto* head);
+
+#endif
